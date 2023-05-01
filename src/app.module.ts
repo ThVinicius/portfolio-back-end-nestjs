de351be49dfad4d619375ce4749e32890ea613bd
Vinicius Pacheco dos Santos <vinicioss9955@gmail.com>
2023-05-01 17:09:02 -0300

    feat: prisma connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProjectModule } from './project/project.module';
+import { PrismaModule } from './database/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [ProjectModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
