afffd2a8f9a8693641238027a56e5401c8d1a44b
Vinicius Pacheco dos Santos <vinicioss9955@gmail.com>
2023-05-06 11:34:50 -0300

    test(feat): project add

--- src/project/__tests__/project.service.spec.ts
+++ src/project/__tests__/project.service.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProjectService } from '../project.service';
 import { PrismaProjectRepository } from '../../database/repositories/project.repository';
+import { ProjectTypeEnum } from '../enums/type.enum';
+import { ProjectLinkEnum } from '../enums/link.enum';
 
 describe('ProjectService', () => {
   let service: ProjectService;
 
   const projectRepositoryMock = {
     getAll: jest.fn(),
+    add: jest.fn(),
   };
 
   beforeEach(async () => {
@@ -30,6 +33,27 @@ describe('ProjectService', () => {
 
       expect(projectRepositoryMock.getAll).toBeCalled();
       expect(result).toBe(projects);
+    });
+  });
+
+  describe('add', () => {
+    it('should add project', async () => {
+      const payload = {
+        name: 'test',
+        type: ProjectTypeEnum.FullStack,
+        description: 'test',
+        image: 'https://google.com',
+        badges: ['react', 'node'],
+        links: [{ label: ProjectLinkEnum.Deploy, href: 'https://google.com' }],
+      };
+      const project = { id: 1, ...payload };
+
+      projectRepositoryMock.add.mockResolvedValueOnce(project);
+
+      const result = await service.add(payload);
+
+      expect(projectRepositoryMock.add).toBeCalledTimes(1);
+      expect(result).toBe(project);
     });
   });
 });
