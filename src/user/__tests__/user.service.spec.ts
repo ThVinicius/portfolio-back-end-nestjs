de47d5e69b7aeba971ab78a5593c87a91f25e481
Vinicius Pacheco dos Santos <vinicioss9955@gmail.com>
2023-05-11 15:22:40 -0300

    test(refactor): exchange for spyOn

--- src/user/__tests__/user.service.spec.ts
+++ src/user/__tests__/user.service.spec.ts
@@ -1,26 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from '../user.service';
 import { PrismaUserRepository } from '../../database/repositories/user.repository';
+import { PrismaModule } from '../../database/prisma.module';
 
 describe('UserService', () => {
   let service: UserService;
-  const userRepositoryMock = { findByUsername: jest.fn() };
+  let userRepository: PrismaUserRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        UserService,
-        { provide: PrismaUserRepository, useValue: userRepositoryMock },
-      ],
+      imports: [PrismaModule],
+      providers: [PrismaUserRepository, UserService],
     }).compile();
 
+    userRepository = module.get<PrismaUserRepository>(PrismaUserRepository);
     service = module.get<UserService>(UserService);
   });
 
-  afterEach(() => jest.clearAllMocks());
-
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(userRepository).toBeDefined();
   });
 
   describe('findByUsername', () => {
@@ -33,12 +32,12 @@ describe('UserService', () => {
         updated_at: new Date(),
       };
 
-      userRepositoryMock.findByUsername.mockResolvedValueOnce(user);
+      jest.spyOn(userRepository, 'findByUsername').mockResolvedValueOnce(user);
 
       const response = await service.findByUsername(user.username);
 
-      expect(userRepositoryMock.findByUsername).toBeCalledTimes(1);
-      expect(userRepositoryMock.findByUsername).toBeCalledWith(user.username);
+      expect(userRepository.findByUsername).toBeCalledTimes(1);
+      expect(userRepository.findByUsername).toBeCalledWith(user.username);
 
       expect(response).toBe(user);
     });
@@ -46,12 +45,12 @@ describe('UserService', () => {
     it('should return null if you pass a username not registered in the database', async () => {
       const username = 'not-registered';
 
-      userRepositoryMock.findByUsername.mockResolvedValueOnce(null);
+      jest.spyOn(userRepository, 'findByUsername').mockResolvedValueOnce(null);
 
       const response = await service.findByUsername(username);
 
-      expect(userRepositoryMock.findByUsername).toBeCalledTimes(1);
-      expect(userRepositoryMock.findByUsername).toBeCalledWith(username);
+      expect(userRepository.findByUsername).toBeCalledTimes(1);
+      expect(userRepository.findByUsername).toBeCalledWith(username);
 
       expect(response).toBe(null);
     });
